5f013c1ac168f9c988c60a98884d58868e533f63
Matthew Dargan <matthewdargan57@gmail.com>
2023-06-11 10:16:21 -0500

    Refactor eBay Finding API request using http.NewRequest and http.Client

--- ebay-find-by-keyword/finding/finding.go
+++ ebay-find-by-keyword/finding/finding.go
@@ -1,25 +1,40 @@
 package finding
 
 import (
-	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 	"os"
+	"time"
 )
 
-const findingURL = "https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords"
+const (
+	findingURL         = "https://svcs.ebay.com/services/search/FindingService/v1?REST-PAYLOAD"
+	operationName      = "findItemsByKeywords"
+	serviceVersion     = "1.0.0"
+	responseDataFormat = "JSON"
+)
 
 func FindItemsByKeywords(keywords string) ([]byte, error) {
-	appID := os.Getenv("EBAY_APP_ID")
-	escKeywords := url.QueryEscape(keywords)
-	apiURL := fmt.Sprintf("%s&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=%s&RESPONSE-DATA-FORMAT=JSON&keywords=%s", findingURL, appID, escKeywords)
-	resp, err := http.Get(apiURL)
+	req, err := http.NewRequest(http.MethodGet, findingURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	q := req.URL.Query()
+	q.Add("OPERATION-NAME", operationName)
+	q.Add("SERVICE-VERSION", serviceVersion)
+	q.Add("SECURITY-APPNAME", os.Getenv("EBAY_APP_ID"))
+	q.Add("RESPONSE-DATA-FORMAT", responseDataFormat)
+	q.Add("keywords", keywords)
+	req.URL.RawQuery = q.Encode()
+
+	c := &http.Client{
+		Timeout: time.Second * 5,
+	}
+	resp, err := c.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-
 	b, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err

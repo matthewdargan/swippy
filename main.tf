1425de86b7f129bb842ff8866010c603491ae1d2
Matthew Dargan <matthewdargan57@gmail.com>
2023-12-11 21:18:31 -0600

    tofu: add S3 backend and DynamoDB lock

--- main.tf
+++ main.tf
@@ -7,6 +7,48 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "swippy-api-terraform-state"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket                  = aws_s3_bucket.tfstate.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning_enabled" {
+  bucket = aws_s3_bucket.tfstate.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "tflock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "swippy-api-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
 variable "ebay_app_id" {
   description = "eBay App ID"
 }
